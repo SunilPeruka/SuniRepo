349c901ee57d384741a2ad40a2abc3445caa6023
Psunil29 <54531052+Psunil29@users.noreply.github.com>
2019-08-26 15:48:22 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -5,11 +5,7 @@ public class Employee implements Cloneable{
 		
 	}
 	
- public Employee(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
+
 private int id;
  private String name;
 public int getId() {
